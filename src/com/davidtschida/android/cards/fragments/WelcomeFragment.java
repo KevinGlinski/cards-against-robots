5b1f5d821d18af5ff1106a7ddcccdbcb7b131825
KevinGlinski <Kevin.Glinski@inin.com>
2014-08-01 23:53:47 -0400

    adding players name instead of just Player X

--- src/com/davidtschida/android/cards/fragments/WelcomeFragment.java
+++ src/com/davidtschida/android/cards/fragments/WelcomeFragment.java
@@ -1,5 +1,8 @@
 package com.davidtschida.android.cards.fragments;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,19 +17,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by david on 7/27/14.
  */
 public class WelcomeFragment extends CastFragment implements OnMessageReceivedListener, OnCastConnectedListener {
 
-
+    private String mUserName;
     public WelcomeFragment() {
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mUserName = getUsername(inflater.getContext());
+
         return inflater.inflate(R.layout.welcome, container, false);
     }
 
@@ -64,11 +71,34 @@ public class WelcomeFragment extends CastFragment implements OnMessageReceivedLi
         }
     }
 
+    public String getUsername(Context context){
+        AccountManager manager = AccountManager.get(context);
+        Account[] accounts = manager.getAccountsByType("com.google");
+        List<String> possibleEmails = new LinkedList<String>();
+
+        for (Account account : accounts) {
+            // TODO: Check possibleEmail against an email regex or treat
+            // account.name as an email address only for certain account.type values.
+            possibleEmails.add(account.name);
+        }
+
+        if(!possibleEmails.isEmpty() && possibleEmails.get(0) != null){
+            String email = possibleEmails.get(0);
+            String[] parts = email.split("@");
+            if(parts.length > 0 && parts[0] != null)
+                return parts[0];
+            else
+                return null;
+        }else
+            return null;
+    }
+
     @Override
     public void onCastConnected() {
         JSONObject json = new JSONObject();
         try {
             json.put("command", "join");
+            json.put("name", mUserName);
             host.getCastmanager().setOnMessageRecievedListener(this);
             host.getCastmanager().sendMessage(json);
         } catch (JSONException e) {
