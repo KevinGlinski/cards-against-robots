b25e089e7ff47f06ef9d035b2be4cfe9a31fbc8b
KevinGlinski <Kevin.Glinski@inin.com>
2014-08-01 23:59:31 -0400

    adding try block around getting user name, if running on vm this will cause errors

--- src/com/davidtschida/android/cards/fragments/WelcomeFragment.java
+++ src/com/davidtschida/android/cards/fragments/WelcomeFragment.java
@@ -4,6 +4,7 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,8 @@ import java.util.List;
  * Created by david on 7/27/14.
  */
 public class WelcomeFragment extends CastFragment implements OnMessageReceivedListener, OnCastConnectedListener {
+
+    final String TAG = "Welfome Fragment";
 
     private String mUserName;
     public WelcomeFragment() {
@@ -72,25 +75,31 @@ public class WelcomeFragment extends CastFragment implements OnMessageReceivedLi
     }
 
     public String getUsername(Context context){
-        AccountManager manager = AccountManager.get(context);
-        Account[] accounts = manager.getAccountsByType("com.google");
-        List<String> possibleEmails = new LinkedList<String>();
+        try {
+            AccountManager manager = AccountManager.get(context);
+            Account[] accounts = manager.getAccountsByType("com.google");
+            List<String> possibleEmails = new LinkedList<String>();
 
-        for (Account account : accounts) {
-            // TODO: Check possibleEmail against an email regex or treat
-            // account.name as an email address only for certain account.type values.
-            possibleEmails.add(account.name);
+            for (Account account : accounts) {
+                // TODO: Check possibleEmail against an email regex or treat
+                // account.name as an email address only for certain account.type values.
+                possibleEmails.add(account.name);
+            }
+
+            if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
+                String email = possibleEmails.get(0);
+                String[] parts = email.split("@");
+                if (parts.length > 0 && parts[0] != null)
+                    return parts[0];
+                else
+                    return "Unknown";
+            } else
+                return "Unknown";
+
+        }catch (Exception ex){
+            Log.e(TAG,"Exception getting user name: ", ex);
+            return "Unknown";
         }
-
-        if(!possibleEmails.isEmpty() && possibleEmails.get(0) != null){
-            String email = possibleEmails.get(0);
-            String[] parts = email.split("@");
-            if(parts.length > 0 && parts[0] != null)
-                return parts[0];
-            else
-                return null;
-        }else
-            return null;
     }
 
     @Override
